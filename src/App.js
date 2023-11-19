518f247626ee9d63c85c5789b50c6982c3446a58
Shaiyo <edsonmike053@gmail.com>
2023-11-19 18:53:52 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { About } from "./About";
 import { Contact } from "./Contact";
 import { Footer } from "./Footer";
@@ -6,14 +7,15 @@ import { Navabar } from "./Navabar";
 import { Skills } from "./Skills";
 
 function App() {
+  const [data, setData] = useState({ name: "", email: "", message: "" });
+
   return (
     <div className="App">
       <Navabar />
       <HeroSection />
       <About />
       <Skills />
-
-      <Contact />
+      <Contact data={data} setData={setData} />
       <Footer />
     </div>
   );
--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,12 +1,11 @@
 import React from "react";
-import bgImage from "./img/khamkeo-vilaysing-rpVQJbZMw8o-unsplash.jpg";
 
 export const HeroSection = () => {
   return (
-    <section className="hero">
+    <div className="hero">
       <p>Hi, I'm</p>
       <h1>EDSON OCHIENG</h1>
       <p>a React FRONTEND DEVELOPER</p>
-    </section>
+    </div>
   );
 };
